01fe3ebffd838803e7129f5b5404901f1905d7cb
chunyen <panchunyen@gmail.com>
2020-08-15 15:58:42 +0800

    add tree

--- Chapter02/nameList.py
+++ Chapter02/nameList.py
@@ -3,6 +3,6 @@ from bs4 import BeautifulSoup
 
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsOj = BeautifulSoup(html, "lxml")
-nameList = bsOj.findAll("span", {"class":"green"})
+nameList = bsOj.findAll("span", {"class": "green"})
 for name in nameList:
     print(name.get_text())
